3ad808dd2e4518d383d1d730376f1456f47f3e4e
Aureo <aureo.serrano@gmail.com>
2015-09-14 21:53:23 +0200

    Empezando

---
+++ app/src/main/java/com/prueba/aureo/prueba/MainActivity.java
@@ -0,0 +1,61 @@
+package com.prueba.aureo.prueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+
+    private EditText et1,et2;
+    private TextView tv3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.et1);
+        et2 = (EditText)findViewById(R.id.et2);
+        tv3 = (TextView)findViewById(R.id.tv3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sumar(View view)
+    {
+        String v1 = et1.getText().toString();
+        String v2 = et2.getText().toString();
+        int vi1 = Integer.parseInt(v1);
+        int vi2 = Integer.parseInt(v2);
+
+        int suma = vi1+vi2;
+
+        tv3.setText(String.valueOf(suma));
+    }
+}
